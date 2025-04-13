b0775aa2f7b3f2b11e089dd53537a67cd8049c95
Samir Al-Bulushi <albulushii.samir@gmail.com>
2025-04-13 08:56:44 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AirlineReservationConsoleSystem_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
